9276267686c0269f03534caddc64e6e3bcd86361
AiXi <aixisibby@outlook.com>
2019-05-10 20:27:00 +0800

    add TestQuery

--- examples/protobuf/codec/codec_test.cc
+++ examples/protobuf/codec/codec_test.cc
@@ -24,13 +24,31 @@ void Print(const Buffer& buf)
 
 void TestQuery()
 {
-    
+    blaze::Query query;
+    query.set_id(1);
+    query.set_questioner("Xi Ai");
+    query.add_question("What's your name");
+
+    Buffer buf;
+    ProtobufCodec::FillEmptyBuffer(&buf, query);
+    Print(buf);
+
+    const int32_t len = buf.ReadInt32();
+    assert(len == static_cast<int32_t>(buf.ReadableBytes()));
+    ProtobufCodec::ErrorCode error_code = ProtobufCodec::ErrorCode::kNoError;
+    MessagePtr message(ProtobufCodec::Parse(buf.Peek(), len, &error_code));
+    assert(error_code == ProtobufCodec::ErrorCode::kNoError);
+    assert(message != nullptr);
+    assert(message->DebugString() == query.DebugString());
+    std::shared_ptr<blaze::Query> new_query(down_pointer_cast<blaze::Query>(message));
+    assert(new_query != nullptr);
 }
 
 int main()
 {
     GOOGLE_PROTOBUF_VERIFY_VERSION;
-    // TODO    
+    TestQuery();
+    puts("");
     google::protobuf::ShutdownProtobufLibrary();
     return 0;
 }
