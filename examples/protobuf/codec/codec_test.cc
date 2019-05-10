015dfe7fc91c729d3f311dbbddf8fe9153231e3f
AiXi <aixisibby@outlook.com>
2019-05-10 21:12:17 +0800

    add TestAnswer TestEmpty

--- examples/protobuf/codec/codec_test.cc
+++ examples/protobuf/codec/codec_test.cc
@@ -28,7 +28,7 @@ void TestQuery()
     query.set_id(1);
     query.set_questioner("Xi Ai");
     query.add_question("What's your name");
-
+    query.add_question("What time is it now");
     Buffer buf;
     ProtobufCodec::FillEmptyBuffer(&buf, query);
     Print(buf);
@@ -44,10 +44,53 @@ void TestQuery()
     assert(new_query != nullptr);
 }
 
+void TestAnswer()
+{
+    blaze::Answer answer;
+    answer.set_id(1);
+    answer.set_questioner("Xi Ai");
+    answer.set_answerer("www.google.com");
+    answer.add_solution("Xi Ai");
+    answer.add_solution("20:57");
+    Buffer buf;
+    ProtobufCodec::FillEmptyBuffer(&buf, answer);
+    Print(buf);
+    const int32_t len = buf.ReadInt32();
+    assert(len == static_cast<int32_t>(buf.ReadableBytes()));
+    ProtobufCodec::ErrorCode error_code = ProtobufCodec::ErrorCode::kNoError;
+    MessagePtr message(ProtobufCodec::Parse(buf.Peek(), len, &error_code));
+    assert(error_code == ProtobufCodec::ErrorCode::kNoError);
+    assert(message != nullptr);
+    message->PrintDebugString();
+    assert(message->DebugString() == answer.DebugString());
+    std::shared_ptr<blaze::Answer> new_answer(down_pointer_cast<blaze::Answer>(message));
+    assert(new_answer != nullptr);
+}
+
+void TestEmpty()
+{
+    blaze::Empty empty;
+    Buffer buf;
+    ProtobufCodec::FillEmptyBuffer(&buf, empty);
+    Print(buf);
+    const int32_t len = buf.ReadInt32();
+    assert(len == static_cast<int32_t>(buf.ReadableBytes()));
+    ProtobufCodec::ErrorCode error_code = ProtobufCodec::ErrorCode::kNoError;
+    MessagePtr message(ProtobufCodec::Parse(buf.Peek(), len, &error_code));
+    assert(error_code == ProtobufCodec::ErrorCode::kNoError);
+    assert(message != nullptr);
+    message->PrintDebugString();
+    assert(message->DebugString() == empty.DebugString());
+}
+
 int main()
 {
     GOOGLE_PROTOBUF_VERIFY_VERSION;
     TestQuery();
+    puts("");
+    TestAnswer();
+    puts("");
+    TestEmpty();
     puts("");
     google::protobuf::ShutdownProtobufLibrary();
     return 0;
