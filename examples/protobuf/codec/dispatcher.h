27a292bcb8c8489dca77cd18d3903d0866c62f60
AiXi <aixisibby@outlook.com>
2019-05-11 12:37:51 +0800

    add dispatcher test

--- examples/protobuf/codec/dispatcher.h
+++ examples/protobuf/codec/dispatcher.h
@@ -5,4 +5,92 @@
 #ifndef BLAZE_DISPATCHER_H
 #define BLAZE_DISPATCHER_H
 
+#include <map>
+#include <type_traits>
+#include <google/protobuf/message.h>
+#include <blaze/utils/Types.h>
+#include <blaze/net/Callbacks.h>
+
+using MessagePtr = std::shared_ptr<google::protobuf::Message>;
+
+class CallbackBase
+{
+public:
+    CallbackBase() = default;
+    virtual ~CallbackBase() = default;
+
+    virtual void OnMessage(const blaze::net::TcpConnectionPtr& conn,
+                           const MessagePtr& message,
+                           blaze::Timestamp receive_time) const = 0;
+
+    DISABLE_COPY_AND_ASSIGN(CallbackBase);
+};
+
+template <typename T>
+class CallbackT : public CallbackBase
+{
+public:
+    static_assert(std::is_base_of_v<google::protobuf::Message, T>,
+        "T must be derived from google::protobuf::Message.");
+    using ProtobufMessageTCallback = std::function<void (const blaze::net::TcpConnectionPtr&,
+                                                         const std::shared_ptr<T>& message,
+                                                         blaze::Timestamp)>;
+
+    explicit CallbackT(ProtobufMessageTCallback cb) :
+        callback_(std::move(cb))
+    {
+    }
+
+    void OnMessage(const blaze::net::TcpConnectionPtr& conn,
+                   const MessagePtr& message,
+                   blaze::Timestamp receive_time) const override
+    {
+        std::shared_ptr<T> concrete(blaze::down_pointer_cast<T>(message));
+        assert(concrete != nullptr);
+        callback_(conn, concrete, receive_time);
+    }
+
+private:
+    ProtobufMessageTCallback callback_;
+};
+
+class ProtobufDispatcher
+{
+public:
+    using ProtobufMessageCallback = std::function<void (const blaze::net::TcpConnectionPtr&,
+                                                         const MessagePtr& message,
+                                                         blaze::Timestamp)>;
+    explicit ProtobufDispatcher(ProtobufMessageCallback cb) :
+        default_callback_(std::move(cb))
+    {
+    }
+
+    void OnProtobufMessage(const blaze::net::TcpConnectionPtr& conn,
+                           const MessagePtr& message,
+                           blaze::Timestamp receive_time) const
+    {
+        auto iter = callbacks_.find(message->GetDescriptor());
+        if (iter != callbacks_.end())
+        {
+            iter->second->OnMessage(conn, message, receive_time);
+        }
+        else
+        {
+            default_callback_(conn, message, receive_time);
+        }
+    }
+
+    template <typename T>
+    void RegisterMessageCallback(const typename CallbackT<T>::ProtobufMessageTCallback& cb)
+    {
+        std::shared_ptr<CallbackT<T>> pd(new CallbackT<T>(cb));
+        callbacks_[T::descriptor()] = pd;
+    }
+
+private:
+    using CallbackMap = std::map<const google::protobuf::Descriptor*, std::shared_ptr<CallbackBase>>;
+    CallbackMap callbacks_;
+    ProtobufMessageCallback default_callback_;
+};
+
 #endif //BLAZE_DISPATCHER_H
--- examples/protobuf/codec/dispatcher_test.cc
+++ examples/protobuf/codec/dispatcher_test.cc
@@ -2,9 +2,57 @@
 // Created by xi on 19-5-11.
 //
 
+#include <iostream>
+#include "query.pb.h"
 #include "dispatcher.h"
+
+using namespace blaze;
+using namespace blaze::net;
+
+using QueryPtr = std::shared_ptr<Query>;
+using AnswerPtr = std::shared_ptr<Answer>;
+
+void test_down_pointer_cast()
+{
+    std::shared_ptr<google::protobuf::Message> msg(new Query);
+    std::shared_ptr<Query> query(down_pointer_cast<Query>(msg));
+    assert(msg && query);
+    if (!query)
+    {
+        abort();
+    }
+}
+
+void OnQuery(const TcpConnectionPtr& conn, const QueryPtr& message, Timestamp)
+{
+    std::cout << "OnQuery: " << message->GetTypeName() << std::endl;
+}
+
+void OnAnswer(const TcpConnectionPtr& conn, const AnswerPtr& message, Timestamp)
+{
+    std::cout << "OnAnswer: " << message->GetTypeName() << std::endl;
+}
+
+void OnUnknownMessageType(const TcpConnectionPtr& conn, const MessagePtr& message, Timestamp)
+{
+    std::cout << "OnUnknownMessageType: " << message->GetTypeName() << std::endl;
+}
 
 int main()
 {
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+    test_down_pointer_cast();
+    ProtobufDispatcher dispatcher(OnUnknownMessageType);
+    dispatcher.RegisterMessageCallback<Query>(OnQuery);
+    dispatcher.RegisterMessageCallback<Answer>(OnAnswer);
+    TcpConnectionPtr conn;
+    Timestamp t;
+    std::shared_ptr<Query> query(new Query);
+    std::shared_ptr<Answer> answer(new Answer);
+    std::shared_ptr<Empty> empty(new Empty);
+    dispatcher.OnProtobufMessage(conn, query, t);
+    dispatcher.OnProtobufMessage(conn, answer, t);
+    dispatcher.OnProtobufMessage(conn, empty, t);
+    google::protobuf::ShutdownProtobufLibrary();
     return 0;
 }
